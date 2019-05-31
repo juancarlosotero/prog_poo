cfb26a80f7cd212ceaf7a3b5dfcc3185b1b5ca1a
rigsapanama <sistemas@relianceteck.com>
2019-05-30 20:12:38 -0500

    problema1 del  taller 1

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import taller_1.Problema1;
+public class Main {
+    public static void main(String[] args) {
+
+        //intanciar taller
+        Problema1 temp = new Problema1();
+        Problema1 n2 = new Problema1();
+
+
+
+        temp.mostrar();
+        n2.mostrar();
+
+    }
+}
